64d98453cf2010154f819a21163b1b47a8fce18e
Your Guide al <alopez@codeceleste.com>
2024-08-17 03:26:03 -0400

    define api and hook behaviors

---
+++ lib/horizon_sdk/behaviors/hook_behavior.ex
@@ -0,0 +1,17 @@
+defmodule HorizonSdk.HookBehavior do
+  @moduledoc """
+  Horizon hooks are called when specific operations
+  occur in a Horizon space.
+  """
+
+  @callback plugin_attachable?() :: :ok
+
+  @callback on_create_place() :: :ok
+  @callback on_click_place() :: :ok
+  @callback on_hover_place() :: :ok
+  @callback on_delete_place() :: :ok
+
+  @callback on_update_place_data() :: :ok
+
+  @callback on_step_time_forward() :: :ok
+end
