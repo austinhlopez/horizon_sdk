240fc4908b437831e5af4037795b0146adce971d
Your Guide al <alopez@codeceleste.com>
2024-08-26 13:50:16 -0400

    layer, space, and horizon schema

--- test/horizon_sdk_test.exs
+++ test/horizon_sdk_test.exs
@@ -7,8 +7,8 @@ defmodule HorizonSdkTest do
     layer_schema_path = Path.join([File.cwd!(), "json_schemas", "layer_schema.json"])
     space_schema_path = Path.join([File.cwd!(), "json_schemas", "space_schema.json"])
 
-    layer_schema = File.read!(layer_schema_path) |> Jason.decode!() |> Schema.resolve()
-    space_schema = File.read!(space_schema_path) |> Jason.decode!() |> Schema.resolve()
+    _layer_schema = File.read!(layer_schema_path) |> Jason.decode!() |> Schema.resolve()
+    _space_schema = File.read!(space_schema_path) |> Jason.decode!() |> Schema.resolve()
 
     assert true
   end
