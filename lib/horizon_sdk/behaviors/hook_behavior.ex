b9540e40445e6635abe2b596b4bcfe288a0239fd
Al Opez <alopez@codeceleste.com>
2024-09-16 01:54:25 -0300

    fix hooks (#5)

--- lib/horizon_sdk/behaviors/hook_behavior.ex
+++ lib/horizon_sdk/behaviors/hook_behavior.ex
@@ -32,25 +32,32 @@ defmodule HorizonSdk.HookBehavior do
     quote do
       @behaviour HorizonSdk.HookBehavior
 
-      def on_click_place(place_id, state), do: state
-      def on_create_place(place_id, state), do: state
-      def on_delete_place(place_id, state), do: state
-      def on_hover_place(place_id, state), do: state
-      def on_load(state), do: state
+      def plugin_attachable?, do: false
       def on_plugin_attach(state), do: state
-      def on_step_time_forward(timestamp, state), do: state
-      def on_update_place_data(place_id, state), do: state
-      def plugin_attachable?, do: true
 
-      defoverridable on_click_place: 2,
-                     on_create_place: 2,
-                     on_delete_place: 2,
-                     on_hover_place: 2,
-                     on_load: 1,
+      def on_load(state), do: state
+
+      def on_create_place(place_id, state), do: state
+      def on_create_layer_place(layer_id, place_id, layer_place_id, state), do: state
+
+      def on_click_place(place_id, state), do: state
+      def on_hover_place(place_id, state), do: state
+      def on_delete_place(place_id, state), do: state
+
+      def on_update_place_data(place_id, state), do: state
+
+      def on_step_time_forward(timestamp, state), do: state
+
+      defoverridable plugin_attachable?: 0,
                      on_plugin_attach: 1,
-                     on_step_time_forward: 1,
+                     on_load: 1,
+                     on_create_place: 2,
+                     on_create_layer_place: 4,
+                     on_click_place: 2,
+                     on_hover_place: 2,
+                     on_delete_place: 2,
                      on_update_place_data: 2,
-                     plugin_attachable?: 0
+                     on_step_time_forward: 2
     end
   end
 end
