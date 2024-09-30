e92e9276a8790ca2e91319420b137810dc574098
Your Guide al <alopez@codeceleste.com>
2024-09-30 01:30:14 -0300

    prepare state for user and session-specific actions

--- lib/horizon_sdk/structs/api_adapters/space_server_adapter_state.ex
+++ lib/horizon_sdk/structs/api_adapters/space_server_adapter_state.ex
@@ -2,11 +2,19 @@ defmodule HorizonSdk.APIAdapters.SpaceServerAdapterState do
   @moduledoc """
   """
 
-  defstruct [:api_server_pid, :pubsub_module, :hook_channel_topic]
+  defstruct [
+    :api_server_pid,
+    :pubsub_module,
+    :hook_channel_topic,
+    :dest_user_id,
+    :dest_session_id
+  ]
 
   @type t :: %__MODULE__{
           api_server_pid: pid(),
           hook_channel_topic: String.t(),
-          pubsub_module: atom()
+          pubsub_module: atom(),
+          dest_user_id: integer() | nil,
+          dest_session_id: integer() | nil
         }
 end
