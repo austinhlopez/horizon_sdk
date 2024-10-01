5590a5fb15bd455c357b4e4e8bbc97e8b5fddbc6
Your Guide al <alopez@codeceleste.com>
2024-09-30 21:13:10 -0300

    include 'scope' to scope actions

--- lib/horizon_sdk/behaviors/api_behavior.ex
+++ lib/horizon_sdk/behaviors/api_behavior.ex
@@ -11,27 +11,39 @@ defmodule HorizonSdk.ApiBehavior do
 
   alias HorizonSdk.APIAdapterState
 
-  # mabye better to use opts, optionally containing adapter state.
+  # mabye better to use scope, optionally containing adapter state.
 
   # (it's unclear whether adapter state needs to be returned from API behavior)
 
   # space
-  @callback get_space(id :: integer(), user_id :: integer(), adapter :: APIAdapterState.t()) ::
+  @callback get_space(
+              id :: integer(),
+              user_id :: integer(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, result :: map(), adapter :: APIAdapterState.t()}
 
   @callback update_space!(
               space_id :: integer(),
               payload :: map(),
               user :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
   # place
-  @callback create_place(payload :: map(), user_id :: integer(), adapter :: APIAdapterState.t()) ::
+  @callback create_place(
+              payload :: map(),
+              user_id :: integer(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, map(), APIAdapterState.t()}
   @callback create_place_from_feature!(
               feature_params :: map(),
               user :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
@@ -39,63 +51,101 @@ defmodule HorizonSdk.ApiBehavior do
               place_id :: integer(),
               payload :: map(),
               user :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
   @callback update_place(
               id :: integer(),
               payload :: map(),
               user :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
   @callback update_place_from_feature!(
               place :: map(),
               feature_params :: map(),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
-  @callback delete_place!(place :: map(), user :: map(), adapter :: APIAdapterState.t()) ::
+  @callback delete_place!(
+              place :: map(),
+              user :: map(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, APIAdapterState.t()}
-  @callback delete_place(id :: integer(), user :: map(), adapter :: APIAdapterState.t()) ::
+  @callback delete_place(
+              id :: integer(),
+              user :: map(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, APIAdapterState.t()}
 
-  @callback get_place(id :: integer(), user_id :: integer(), adapter :: APIAdapterState.t()) ::
+  @callback get_place(
+              id :: integer(),
+              user_id :: integer(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, map(), APIAdapterState.t()}
   @callback get_places(
               ids :: list(integer()),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, list(map()), APIAdapterState.t()}
 
   # layer
-  @callback create_layer(payload :: map(), user_id :: integer(), adapter :: APIAdapterState.t()) ::
+  @callback create_layer(
+              payload :: map(),
+              user_id :: integer(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, map(), APIAdapterState.t()}
   @callback update_layer(
               id :: integer(),
               user_id :: integer(),
+              scope :: map(),
               payload :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
-  @callback delete_layer(id :: integer(), user_id :: integer(), adapter :: APIAdapterState.t()) ::
+  @callback delete_layer(
+              id :: integer(),
+              user_id :: integer(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, APIAdapterState.t()}
 
-  @callback get_layer(id :: integer(), user_id :: integer(), adapter :: APIAdapterState.t()) ::
+  @callback get_layer(
+              id :: integer(),
+              user_id :: integer(),
+              scope :: map(),
+              adapter :: APIAdapterState.t()
+            ) ::
               {:ok, map(), APIAdapterState.t()}
   @callback get_layers(
               ids :: list(integer()),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, list(map()), APIAdapterState.t()}
 
   @callback get_with_layer_places(
               layer_id :: integer(),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
   @callback set_layer_symbol(
               layer_id :: integer(),
               user_id :: integer(),
+              scope :: map(),
               payload :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, APIAdapterState.t()}
@@ -103,6 +153,7 @@ defmodule HorizonSdk.ApiBehavior do
   @callback set_layer_3d(
               layer_id :: integer(),
               user_id :: integer(),
+              scope :: map(),
               payload :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, APIAdapterState.t()}
@@ -111,6 +162,7 @@ defmodule HorizonSdk.ApiBehavior do
   @callback get_layer_places_by_layer_id(
               layer_id :: integer(),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, list(map()), APIAdapterState.t()}
 
@@ -119,6 +171,7 @@ defmodule HorizonSdk.ApiBehavior do
               place_id :: integer(),
               user_id :: integer(),
               payload :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
@@ -126,6 +179,7 @@ defmodule HorizonSdk.ApiBehavior do
               layer_id :: integer(),
               user_id :: integer(),
               payload :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
@@ -133,6 +187,7 @@ defmodule HorizonSdk.ApiBehavior do
               space_id :: integer(),
               user_id :: integer(),
               payload :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
@@ -147,6 +202,7 @@ defmodule HorizonSdk.ApiBehavior do
   @callback update_circle_radius(
               layer_id :: integer,
               value :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, APIAdapterState.t()}
 
@@ -154,37 +210,42 @@ defmodule HorizonSdk.ApiBehavior do
   @callback get_variation(
               variation_id :: integer(),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
   @callback create_variation(
               payload :: map(),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, APIAdapterState.t()}
 
   @callback set_variation(
               variation_id :: integer(),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) ::
               {:ok, APIAdapterState.t()}
 
   # time
-  @callback disable_step_time_forward(adapter :: APIAdapterState.t()) :: :ok
-  @callback enable_step_time_forward(adapter :: APIAdapterState.t()) :: :ok
-  @callback step_time_forward(adapter :: APIAdapterState.t()) :: :ok
+  @callback disable_step_time_forward(scope :: map(), adapter :: APIAdapterState.t()) :: :ok
+  @callback enable_step_time_forward(scope :: map(), adapter :: APIAdapterState.t()) :: :ok
+  @callback step_time_forward(scope :: map(), adapter :: APIAdapterState.t()) :: :ok
 
   # blocks
   @callback update_block_content(
               id :: String.t(),
               user_id :: integer(),
               payload :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
   @callback update_block_property(
               id :: String.t(),
               user_id :: integer(),
               payload :: map(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, map(), APIAdapterState.t()}
 
@@ -192,22 +253,25 @@ defmodule HorizonSdk.ApiBehavior do
               id :: String.t(),
               user_id :: integer(),
               text :: String.t(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, APIAdapterState.t()}
 
   @callback get_block_text(
               id :: String.t(),
               user_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, String.t(), APIAdapterState.t()}
 
   # tooltip
-  @callback set_tooltip(String.t(), adapter :: APIAdapterState.t()) :: :ok
+  @callback set_tooltip(text :: String.t(), scope :: map(), adapter :: APIAdapterState.t()) :: :ok
 
   # space plugin
   @callback get_space_plugin_by_space_id_plugin_id(
               space_id :: integer(),
               plugin_id :: integer(),
+              scope :: map(),
               adapter :: APIAdapterState.t()
             ) :: {:ok, result :: map(), APIAdapterState.t()}
 
@@ -215,111 +279,111 @@ defmodule HorizonSdk.ApiBehavior do
     quote do
       @behaviour HorizonSdk.ApiBehavior
 
-      def get_space(id, user_id, adapter), do: {:ok, %{}, adapter}
+      def get_space(id, user_id, scope, adapter), do: {:ok, %{}, adapter}
 
-      def update_space!(space_id, params, user_id, adapter), do: {:ok, params, adapter}
+      def update_space!(space_id, params, user_id, scope, adapter), do: {:ok, params, adapter}
 
-      def create_place(payload, user_id, adapter), do: {:ok, %{}, adapter}
+      def create_place(payload, user_id, scope, adapter), do: {:ok, %{}, adapter}
 
-      def create_place_from_feature!(feature_params, current_user, adapter),
+      def create_place_from_feature!(feature_params, current_user, scope, adapter),
         do: {:ok, %{}, adapter}
 
-      def update_place!(id, payload, user, adapter), do: {:ok, payload, adapter}
-      def update_place(id, payload, user, adapter), do: {:ok, payload, adapter}
+      def update_place!(id, payload, user, scope, adapter), do: {:ok, payload, adapter}
+      def update_place(id, payload, user, scope, adapter), do: {:ok, payload, adapter}
 
-      def update_place_from_feature!(selected_place, feature_params, current_user, adapter),
+      def update_place_from_feature!(selected_place, feature_params, current_user, scope, adapter),
         do: {:ok, feature_params, adapter}
 
-      def delete_place!(place, user, adapter), do: {:ok, adapter}
-      def delete_place(id, user, adapter), do: {:ok, adapter}
+      def delete_place!(place, user, scope, adapter), do: {:ok, adapter}
+      def delete_place(id, user, scope, adapter), do: {:ok, adapter}
 
-      def get_place(id, user_id, adapter), do: {:ok, %{}, adapter}
-      def get_places(ids, user_id, adapter), do: {:ok, [], adapter}
+      def get_place(id, user_id, scope, adapter), do: {:ok, %{}, adapter}
+      def get_places(ids, user_id, scope, adapter), do: {:ok, [], adapter}
 
       # layer
-      def create_layer(payload, user_id, adapter), do: {:ok, %{}, adapter}
-      def update_layer(id, user_id, payload, adapter), do: {:ok, %{}, adapter}
-      def delete_layer(id, user_id, adapter), do: {:ok, adapter}
+      def create_layer(payload, user_id, scope, adapter), do: {:ok, %{}, adapter}
+      def update_layer(id, user_id, payload, scope, adapter), do: {:ok, %{}, adapter}
+      def delete_layer(id, user_id, scope, adapter), do: {:ok, adapter}
 
-      def get_layer(id, user_id, adapter), do: {:ok, %{}, adapter}
-      def get_layers(ids, user_id, adapter), do: {:ok, [], adapter}
+      def get_layer(id, user_id, scope, adapter), do: {:ok, %{}, adapter}
+      def get_layers(ids, user_id, scope, adapter), do: {:ok, [], adapter}
 
-      def get_with_layer_places(layer_id, user_id, adapter), do: {:ok, %{}, adapter}
+      def get_with_layer_places(layer_id, user_id, scope, adapter), do: {:ok, %{}, adapter}
 
-      def set_layer_symbol(layer_id, user_id, payload, adapter), do: {:ok, adapter}
-      def set_layer_3d(layer_id, user_id, payload, adapter), do: {:ok, adapter}
+      def set_layer_symbol(layer_id, user_id, payload, scope, adapter), do: {:ok, adapter}
+      def set_layer_3d(layer_id, user_id, payload, scope, adapter), do: {:ok, adapter}
 
       # layer places
-      def get_layer_places_by_layer_id(layer_id, user_id, adapter), do: {:ok, [], adapter}
+      def get_layer_places_by_layer_id(layer_id, user_id, scope, adapter), do: {:ok, [], adapter}
 
-      def update_place_data(place_id, user_id, payload, adapter), do: {:ok, %{}, adapter}
-      def update_layer_data(layer_id, user_id, payload, adapter), do: {:ok, %{}, adapter}
-      def update_space_data(space_id, user_id, payload, adapter), do: {:ok, %{}, adapter}
+      def update_place_data(place_id, user_id, payload, scope, adapter), do: {:ok, %{}, adapter}
+      def update_layer_data(layer_id, user_id, payload, scope, adapter), do: {:ok, %{}, adapter}
+      def update_space_data(space_id, user_id, payload, scope, adapter), do: {:ok, %{}, adapter}
 
-      def set_color(adapter), do: :ok
-      def set_size(adapter), do: :ok
-      def update_circle_radius(layer_id, value, adapter), do: {:ok, adapter}
+      def set_color(scope, adapter), do: :ok
+      def set_size(scope, adapter), do: :ok
+      def update_circle_radius(layer_id, value, scope, adapter), do: {:ok, adapter}
 
-      def get_variation(variation_id, user_id, adapter), do: {:ok, %{}, adapter}
-      def create_variation(payload, user_id, adapter), do: {:ok, %{}, adapter}
-      def set_variation(variation_id, user_id, adapter), do: {:ok, adapter}
+      def get_variation(variation_id, user_id, scope, adapter), do: {:ok, %{}, adapter}
+      def create_variation(payload, user_id, scope, adapter), do: {:ok, %{}, adapter}
+      def set_variation(variation_id, user_id, scope, adapter), do: {:ok, adapter}
 
       # time
-      def disable_step_time_forward(adapter), do: {:ok, adapter}
-      def enable_step_time_forward(adapter), do: {:ok, adapter}
-      def step_time_forward(adapter), do: {:ok, adapter}
+      def disable_step_time_forward(scope, adapter), do: {:ok, adapter}
+      def enable_step_time_forward(scope, adapter), do: {:ok, adapter}
+      def step_time_forward(scope, adapter), do: {:ok, adapter}
 
       # blocks
-      def update_block_content(id, user_id, payload, adapter), do: {:ok, %{}, adapter}
-      def update_block_property(id, user_id, payload, adapter), do: {:ok, %{}, adapter}
+      def update_block_content(id, user_id, payload, scope, adapter), do: {:ok, %{}, adapter}
+      def update_block_property(id, user_id, payload, scope, adapter), do: {:ok, %{}, adapter}
 
-      def set_block_text(id, user_id, text, adapter), do: {:ok, "", adapter}
-      def get_block_text(id, user_id, adapter), do: {:ok, "", adapter}
+      def set_block_text(id, user_id, text, scope, adapter), do: {:ok, "", adapter}
+      def get_block_text(id, user_id, scope, adapter), do: {:ok, "", adapter}
 
       # tooltips
-      def set_tooltip(_, adapter), do: :ok
+      def set_tooltip(_, scope, adapter), do: :ok
 
       # space plugin
-      def get_space_plugin_by_space_id_plugin_id(_, _, adapter), do: {:ok, %{}, adapter}
+      def get_space_plugin_by_space_id_plugin_id(_, _, scope, adapter), do: {:ok, %{}, adapter}
 
-      defoverridable get_space: 3,
-                     update_space!: 4,
-                     create_place: 3,
-                     create_place_from_feature!: 3,
-                     update_place!: 4,
-                     update_place: 4,
-                     update_place_from_feature!: 4,
-                     delete_place!: 3,
-                     delete_place: 3,
-                     get_place: 3,
-                     get_places: 3,
-                     create_layer: 3,
-                     update_layer: 4,
-                     delete_layer: 3,
-                     get_layer: 3,
-                     get_layers: 3,
-                     get_with_layer_places: 3,
-                     set_layer_symbol: 4,
-                     set_layer_3d: 4,
-                     get_layer_places_by_layer_id: 3,
-                     update_place_data: 4,
-                     update_layer_data: 4,
-                     update_space_data: 4,
-                     set_color: 1,
-                     set_size: 1,
-                     update_circle_radius: 3,
-                     get_variation: 3,
-                     create_variation: 3,
-                     set_variation: 3,
-                     disable_step_time_forward: 1,
-                     enable_step_time_forward: 1,
-                     step_time_forward: 1,
-                     update_block_content: 4,
-                     update_block_property: 4,
-                     set_block_text: 4,
-                     get_block_text: 3,
-                     set_tooltip: 2,
-                     get_space_plugin_by_space_id_plugin_id: 3
+      defoverridable get_space: 4,
+                     update_space!: 5,
+                     create_place: 4,
+                     create_place_from_feature!: 4,
+                     update_place!: 5,
+                     update_place: 5,
+                     update_place_from_feature!: 5,
+                     delete_place!: 4,
+                     delete_place: 4,
+                     get_place: 4,
+                     get_places: 4,
+                     create_layer: 4,
+                     update_layer: 5,
+                     delete_layer: 4,
+                     get_layer: 4,
+                     get_layers: 4,
+                     get_with_layer_places: 4,
+                     set_layer_symbol: 5,
+                     set_layer_3d: 5,
+                     get_layer_places_by_layer_id: 4,
+                     update_place_data: 5,
+                     update_layer_data: 5,
+                     update_space_data: 5,
+                     set_color: 2,
+                     set_size: 2,
+                     update_circle_radius: 4,
+                     get_variation: 4,
+                     create_variation: 4,
+                     set_variation: 4,
+                     disable_step_time_forward: 2,
+                     enable_step_time_forward: 2,
+                     step_time_forward: 2,
+                     update_block_content: 5,
+                     update_block_property: 5,
+                     set_block_text: 5,
+                     get_block_text: 4,
+                     set_tooltip: 3,
+                     get_space_plugin_by_space_id_plugin_id: 4
     end
   end
 end
